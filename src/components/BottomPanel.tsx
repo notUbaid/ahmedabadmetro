ea6afbca1aac97f7e52ca457b4f9aedfa8170298
Ubaid <me.khanubaid@gmail.com>
2026-06-09 16:59:49 +0530

    fix: limit upcoming trains in bottom panel to 2

--- src/components/BottomPanel.tsx
+++ src/components/BottomPanel.tsx
@@ -60,10 +60,10 @@ export const BottomPanel = ({
   // Auto-refresh upcoming trains and last train warnings every 10 seconds
   useEffect(() => {
     if (station) {
-      setUpcomingTrains(getUpcomingTrains(station.id, 3));
+      setUpcomingTrains(getUpcomingTrains(station.id, 2));
       setLastTrainWarnings(getLastTrainWarnings(station.id));
       const interval = setInterval(() => {
-        setUpcomingTrains(getUpcomingTrains(station.id, 3));
+        setUpcomingTrains(getUpcomingTrains(station.id, 2));
         setLastTrainWarnings(getLastTrainWarnings(station.id));
       }, 10000);
       return () => clearInterval(interval);
